271c1530a3ab35e788357aab32585da1fd3ea27a
COllins <collinsmuiruri77@gmail.com>
2022-10-06 23:37:16 +0300

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import "/home/lenovo/Development/imports-by-collo/src/App.css";
 import HeroSection from "../HeroSection";
 import React from 'react';
+import Footer from "../Footer";
 
 
 function Home() {
   return (
    <>
    <HeroSection/>
+   <Footer/>
    </>
   )
 }
